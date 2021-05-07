459a8c97d761a9fce9ac4c1b4230232c7aed06e4
Mustafa Fındık <mustafa.findik3@gmail.com>
2021-05-07 09:05:55 +0300

    AppSetting.json Database Values added and DatabaseSettings added and StartUp Conf. added

---
+++ Services/Catalog/Course.Services.Catalog/Utilities/Settings/DatabaseSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.Services.Catalog.Utilities.Settings
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string CourseCollectionName { get; set; }
+        public string CategoryCollectionName { get; set; }
+    }
+}
