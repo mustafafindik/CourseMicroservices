fe35a33c3597d9fac48a92ade5b5eb91833e29e8
mustafafindik <mustafa.findik3@gmail.com>
2021-05-10 15:46:47 +0300

    Photo Stock Api added

---
+++ Services/PhotoStock/Course.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,46 @@
+﻿using Course.Services.PhotoStock.Dtos;
+using Course.Services.PhotoStock.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Course.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSaveAsync(IFormFile photo, CancellationToken cancellationToken)
+        {
+
+            if (photo != null && photo.Length > 0)
+            {             
+                var filepath =  await FileHelper.Task<string>(photo, cancellationToken);
+                PhotoDto photoDto = new() { Url = filepath };
+
+                return Ok(photoDto);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            
+            if (!FileHelper.FileExist(photoUrl))
+            {
+                return BadRequest("Resim yok");
+            }
+
+            FileHelper.Delete(photoUrl);
+
+            return Ok();
+        }
+    }
+}
---
+++ Services/PhotoStock/Course.Services.PhotoStock/Helpers/FileHelper.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Course.Services.PhotoStock.Helpers
+{
+    public class FileHelper
+    {
+        public async static Task<string> Task<Add>(IFormFile image, CancellationToken cancellationToken)
+        {
+            try
+            {
+                string directory = Environment.CurrentDirectory + @"\wwwroot\";
+                var fileName = CreateNewFileName(image.FileName);
+
+                string path = Path.Combine(directory, "photos");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+                {
+                    await image.CopyToAsync(stream, cancellationToken);
+                }
+
+                var filePath = Path.Combine(path, fileName);
+                var url = "photos/" + fileName;
+                return url;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
+
+        public static string CreateNewFileName(string fileName)
+        {
+            string[] file = fileName.Split('.');
+            string extension = file[1];
+            string newFileName = string.Format(@"{0}." + extension, Guid.NewGuid());
+            return newFileName;
+        }
+
+        public static bool Delete(string photoUrl)
+        {
+            try
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+                File.Delete(path);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+
+        public static bool FileExist(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoUrl);
+            if (!System.IO.File.Exists(path))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
